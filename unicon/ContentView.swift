bb2749d50df5594891e6113a87e9b81f1a9e1c63
otofune <otofune@otofune.me>
2020-11-17 22:18:05 +0900

    Initial Commit

---
+++ unicon/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  unicon
+//
+//  Created by owner on 2020/11/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
